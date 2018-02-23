2bd872cf23a580bca2d2cef5da89595c1151d455
Unknown <ro.wemmers@gmail.com>
2018-02-23 13:21:52 +0100

    Auto stash before merge of "Roy" and "master"

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>LogboekApp</title>
+</head>
+<body>
+    <form action="inc/login.php">
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email-for">
+
+        <label for="pwd">Password</label>
+        <input type="password" name="pwd" id="pwd">
+
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
