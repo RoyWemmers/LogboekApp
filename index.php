2acb0810266e7b913a1015cb879ac48e81959bf4
jmonztr <jules.mons00@gmail.com>
2018-02-23 13:29:02 +0100

    css aangemaakt + form aangepast

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <title>LogboekApp</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <form action="inc/login.php">
+    <form action="inc/login.php" method="POST">
         <label for="email">Email</label>
         <input type="email" name="email" id="email-for">
 
